cb43b110f42e20d14e0f3acc7b94665801d33142
Hadil kortas <kortashadil27@gmail.com>
2023-11-19 21:19:47 +0100

    Initial commit

---
+++ src/test/java/com/task/taskManagement/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.taskManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
