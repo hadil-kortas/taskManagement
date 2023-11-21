7be15060a6bcdaf4308165f0cd61abe1efd134ba
Hadil kortas <kortashadil27@gmail.com>
2023-11-21 17:17:50 +0100

    update

--- src/main/java/com/task/taskManagement/TaskManagementApplication.java
+++ src/main/java/com/task/taskManagement/TaskManagementApplication.java
@@ -6,6 +6,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Example;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.Date;
 
 @SpringBootApplication
 @AllArgsConstructor
@@ -28,6 +33,35 @@ public class TaskManagementApplication implements CommandLineRunner {
 		user.setUsername("hadil_kortas");
 		user.setPassword("hadil123");
 		userRepository.save(user);
+
+
+		User user2 = new User();
+		user2.setUsername("ons_belhaj");
+		user2.setPassword("onsons");
+		userRepository.save(user2);
+
+
+		// Example of creating a new team
+		Team team = new Team();
+		team.setName("tigers");
+		team.getUsers().add(user2);
+		teamRepository.save(team);
+
+		Team team1 = new Team();
+		team1.setName("A");
+		team1.getUsers().add(user);
+		teamRepository.save(team1);
+
+		// Example of creating a new task
+		Task task = new Task();
+		task.setTitle("Create an admin dashboard");
+		task.setDescription("gdgsdsdgsgsdgds");
+		task.setTeam(team);
+		LocalDate d = LocalDate.of(2022,11,12);
+		task.setDueDate(d);
+		taskRepository.save(task);
+
+
 
 	}
 }
