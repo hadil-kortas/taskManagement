3b8e56e15626ba4a8423e48388e875b1c7084233
Hadil kortas <kortashadil27@gmail.com>
2023-11-25 17:42:51 +0100

    update

--- src/main/java/com/task/taskManagement/controller/TeamController.java
+++ src/main/java/com/task/taskManagement/controller/TeamController.java
@@ -1,4 +1,14 @@
 package com.task.taskManagement.controller;
 
+
+import com.task.taskManagement.service.ServiceTeam;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@AllArgsConstructor
+
 public class TeamController {
+
+    ServiceTeam serviceTeam;
 }
